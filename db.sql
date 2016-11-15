188afc654fe7ba2d73a53d125825714d1c83f5b9
rmenn <rahul@rmenn.in>
2016-11-15 13:45:24 +0530

    sql seed

---
+++ db.sql
@@ -0,0 +1,21 @@
+SET foreign_key_checks = 1;
+SET time_zone = '+05:30';
+
+CREATE DATABASE IF NOT EXISTS atmdis DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+
+USE atmdis;
+
+CREATE TABLE Atms (
+    id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+    location VARCHAR(25) NOT NULL,
+    city VARCHAR(25) NOT NULL,
+    bank VARCHAR(25) NOT NULL,
+    created_at TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO `Atms` (`id`, `location`, `bank`, `city`) VALUES
+(1, 'Raheja Arcade, Koramangla', 'Citibank', 'Bangalore'),
+(2, '80Feet Road, Koramangla', 'Axis', 'Bangalore');
+
